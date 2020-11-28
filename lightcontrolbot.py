6f5d67416e7e1781e4127f6ba014e39b8e0d4f63
Arun-18 <75080710+Arun-18@users.noreply.github.com>
2020-11-28 17:13:27 +0530

    Update lightcontrolbot.py

--- lightcontrolbot.py
+++ lightcontrolbot.py
@@ -1,52 +1,35 @@
-from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
-from Adafruit_IO import Client,Feed,Data
-import os
+!pip install adafruit-io
+x = "Kumar13"  #ADAFRUIT_IO_USERNAME
+y = "aio_UHZA63kUi0x1anfYfeJwWG9gQ82w" #ADAFRUIT_IO_KEY
+from Adafruit_IO import Client, Feed
+aio = Client(x,y)
+# create feed
+feed=Feed(name='bot118') # Feed name is given
+result = aio.create_feed(feed)
+result
+from Adafruit_IO import Data
+#sending a value to a feed
+value=Data(value=0) 
+value_send=aio.create_data('bot',value)
+# Telegram API
+# 1.create telegram bot
+# 2.send a query
+!pip install python-telegram-bot
+from telegram.ext import Updater,CommandHandler
+import requests #getting data from the cloud
 
-ADAFRUIT_IO_USERNAME = os.getenv('ADAFRUIT_IO_USERNAME')
-ADAFRUIT_IO_KEY = os.getenv('ADAFRUIT_IO_KEY')
-aio = Client('ADAFRUIT_IO_USERNAME','ADAFRUIT_IO_KEY')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+def get_url():
+    contents=requests.get('https://random.dog/woof.json').json()
+    url=contents['url']
+    return url
 
-def start(bot, update):
-    print(str( update.effective_chat.id ))
-    bot.send_message(chat_id = update.effective_chat.id, text="Welcome! Type 'Turn on the Light' or /lighton to switch on the light bulb. Type 'Turn off the Light' or /lightoff to switch off the light bulb.")
+def dog(bot,update):
+    url=get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id,photo=url)
 
-def unknown(bot, update):
-    bot.send_message(chat_id=update.effective_chat.id, text="Oops, I didn't understand that. Try again!")
-
-
-def value_send(value):
-  to_feed = aio.feeds('lightbotctrl')
-  aio.send_data(to_feed.key,value)
-
-def lighton(bot, update):
-  chat_id = update.message.chat_id
-  bot.send_message(chat_id, text="Light has been turned ON")
-  bot.send_photo(chat_id, photo='https://www.securityroundtable.org/wp-content/uploads/2019/03/AdobeStock_261504199-scaled.jpeg')
-  value_send(1)
-
-def lightoff(bot, update):
-  chat_id = update.message.chat_id
-  bot.send_message(chat_id, text="Light has been turned OFF")
-  bot.send_photo(chat_id=update.effective_chat.id,photo='https://ak.picdn.net/shutterstock/videos/1027638404/thumb/1.jpg?ip=x480')
-  value_send(0)
-
-def given_message(bot, update):
-  text = update.message.text.upper()
-  text = update.message.text
-  if text == 'Turn on the Light':
-    lighton(bot,update)
-  
-  elif text == 'Turn off the Light':
-    lightoff(bot,update)
-
-u = Updater('TELEGRAM_TOKEN',use_context = True)
+u = Updater('1400818256:AAHC211LcAFE2wd2JaRS1AVzRv7x_i9unZ8')
 dp = u.dispatcher
-dp.add_handler(CommandHandler('lighton',lighton))
-dp.add_handler(CommandHandler('lightoff',lightoff))
-dp.add_handler(CommandHandler('start', start))
-dp.add_handler(MessageHandler(Filters.command, unknown))
-dp.add_handler(MessageHandler(Filters.text, given_message))
-
+dp.add_handler(CommandHandler('dog',dog))
 u.start_polling()
 u.idle()
